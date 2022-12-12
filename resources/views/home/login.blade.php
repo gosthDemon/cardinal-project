e45abd49f44c3a1b01002f7da0d2a0f949e38448
aldahir <aldahirruizvaldez0809@gmail.com>
2022-12-12 17:46:58 -0400

    add logic for login v1

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -15,65 +15,86 @@
 </head>
 
 <body>
-    <section class="login-content-form">
-        <div class="card-one">
-            <div class="content">
-                <header>
-                    <div class="tittle-form">
-                        <h2 class="tittle-cardinal">Sistema Cardinal</h2>
-                        <h2 class="tittle-sistem">Registro de Seguimiento Académico</h2>
-                    </div>
-                    <div class="logo-form">
-                        <img src="{{ URL::asset('img/cardinal/login/user.png') }}" alt="">
-                    </div>
-                </header>
-                <div class="body">
-                    <div class="group-form">
-                        <label for="user">Usuario:</label>
-                        <input type="text" id="user" name="user" placeholder="Ingrese su usuario" required>
-                    </div>
-                    <div class="group-form">
-                        <label for="password">Contraseña:</label>
-                        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña"
-                            required>
-                        <div class="row">
-                            <input type="checkbox" id="show_password" name="show_password" class="show-password"
-                                required> <label for="show_password">&nbsp; Ver contraseña</label>
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <section class="login-content-form">
+            <div class="card-one">
+                <div class="content">
+
+                    <header>
+                        <div class="tittle-form">
+                            <h2 class="tittle-cardinal">Sistema Cardinal</h2>
+                            <h2 class="tittle-sistem">Registro de Seguimiento Académico</h2>
                         </div>
+                        <div class="logo-form">
+                            <img src="{{ URL::asset('img/cardinal/login/user.png') }}" alt="">
+                        </div>
+                    </header>
+                    <div class="body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="group-form">
+                                <label for="email">Usuario:</label>
+                                <input type="email" id="email" name="email" value="{{ old('email', '') }}"
+                                    placeholder="Ingrese su usuario" required>
+                            </div>
+                            <div class="group-form">
+                                <label for="password">Contraseña:</label>
+                                <input type="password" id="password" name="password"
+                                    placeholder="Ingrese su contraseña" required>
+                                <div class="row">
+                                    <input type="checkbox" id="show_password" name="show_password"
+                                        class="show-password"> <label for="show_password">&nbsp; Ver contraseña</label>
+                                </div>
+                            </div>
+                            {{-- <div class="block mt-4">
+                            <label for="remember_me" class="inline-flex items-center">
+                                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            </label>
+                        </div> --}}
+                            <div class="group-form">
+                                    <div class="error-form-input">&nbsp; @error('email'){{ $message }}@enderror</div>
+                            </div>
+                            <div class="group-form">
+                                <button class="login-button">Iniciar Sesión</button>
+                            </div>
+
+                            <div class="group-form">
+                                <a href="#" class="forgot-password" id="forgot_password"
+                                    name="forgot_password">¿Ha
+                                    olvidado su contraseña?</a>
+                            </div>
+                        </form>
                     </div>
-                    <div class="group-form">
-                        <button class="login-button">Iniciar Sesión</button>
-                    </div>
-                    <div class="group-form">
-                        <a href="#" class="forgot-password" id="forgot_password" name="forgot_password">¿Ha
-                            olvidado su contraseña?</a>
-                    </div>
+
                 </div>
             </div>
-        </div>
-        <div class="card-two">
-        </div>
-    </section>
-    <script>
-        let show_password = document.getElementById('show_password');
-        let password = document.getElementById('password');
+            <div class="card-two">
+            </div>
+        </section>
 
-        show_password.addEventListener('click', function() {
-            if (show_password.checked) {
-                if (password.type === 'password') {
-                    password.type = "text";
+        <script>
+            let show_password = document.getElementById('show_password');
+            let password = document.getElementById('password');
+
+            show_password.addEventListener('click', function() {
+                if (show_password.checked) {
+                    if (password.type === 'password') {
+                        password.type = "text";
+                    } else {
+                        password.type = "password";
+                    }
                 } else {
-                    password.type = "password";
+                    if (password.type === 'password') {
+                        password.type = "text";
+                    } else {
+                        password.type = "password";
+                    }
                 }
-            } else {
-                if (password.type === 'password') {
-                    password.type = "text";
-                } else {
-                    password.type = "password";
-                }
-            }
-        });
-    </script>
+            });
+        </script>
 </body>
 
 </html>
