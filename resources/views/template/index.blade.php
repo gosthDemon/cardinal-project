72a00abadf91eb68967d38d1303c4e2184501861
aldahir <aldahirruizvaldez0809@gmail.com>
2022-12-24 11:52:31 -0400

    add table v1
    add form v1

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ URL::asset('css/cardinal.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/fontAwesome.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::asset('css/cardinal.css') }}">
     <title>Mis Cursos</title>
 </head>
 
